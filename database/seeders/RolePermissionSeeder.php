139097b147e2f23490c19da3b52520ac5711639d
Rio Oktaviansyah <riooktaviansyah36@gmail.com>
2023-12-02 10:57:04 +0700

    install spatie install breeze

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Permission::create(['name'=> 'add-user']);
+        Permission::create(['name'=> 'edit-user']);
+        Permission::create(['name'=> 'delete-user']);
+        Permission::create(['name'=> 'view-user']);
+
+        Role::create(['name'=> 'admin']);
+        Role::create(['name'=> 'user']);
+
+        $roleAdmin = Role::findByName('admin');
+        $roleAdmin->givePermissionTo('add-user');
+        $roleAdmin->givePermissionTo('edit-user');
+        $roleAdmin->givePermissionTo('delete-user');
+        $roleAdmin->givePermissionTo('view-user');
+
+        $roleUser = Role::findByName('user');
+        $roleUser->givePermissionTo('view-user');
+    }
+}
